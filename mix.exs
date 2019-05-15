2dd54f4be1be799a7a5040f0ad8269da468695cd
Coby Benveniste <coby.b@spot.im>
2019-05-15 08:12:45 +0300

    Add formatter.exs, test folder, update mix.exs with general testing and development dependencies (Basic Elixir Package Building Blocks)

--- mix.exs
+++ mix.exs
@@ -1,15 +1,62 @@
 defmodule Stargate.Mixfile do
   use Mix.Project
 
-  def project,
-    do: [
+  def project do
+    [
       app: :stargate,
       version: "0.0.1",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      name: "Stargate",
+      source_url: "https://github.com/coby-spotim/stargate",
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env()),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      # The main page in the docs
+      docs: [main: "Stargate", extras: ["README.md"]],
+      dialyzer: [plt_add_deps: :project]
     ]
+  end
 
-  def deps, do: []
+  def application do
+    [
+      extra_applications: applications(Mix.env())
+    ]
+  end
+
+  defp applications(:dev), do: applications(:all) ++ [:remixed_remix]
+  defp applications(_all), do: [:logger, :runtime_tools]
+
+  def deps do
+    [
+      ## Testing and Development Dependencies
+      {:ex_doc, "~> 0.18.0", only: :dev},
+      {:credo, "~> 1.0.0", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:remixed_remix, "~> 1.0.0", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      description: "High Performance Low Level Elixir Webserver",
+      # These are the default files included in the package
+      files: ["lib", "mix.exs", "README.md"],
+      maintainers: ["Coby Benveniste"],
+      links: %{"GitHub" => "https://github.com/coby-spotim/stargate"},
+      licenses: ["GNU GPL License"]
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end
---
+++ test/test_helper.exs
@@ -0,0 +1,2 @@
+ExUnit.configure(exclude: [:disabled, :disable, :excluded, :exclude, :skipped, :skip])
+ExUnit.start()
