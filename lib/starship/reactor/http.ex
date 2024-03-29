9f085627b2f1ba1cca359e73bc2af2700996cb3d
Coby Benveniste <coby.benveniste@gmail.com>
2024-03-29 12:36:20 +0300

    Replace apply/3 with simple module call

--- lib/starship/reactor/http.ex
+++ lib/starship/reactor/http.ex
@@ -11,7 +11,7 @@ defmodule Starship.Reactor.Http do
   def handle_http_request(%Conn{http_version: http_version} = conn, config) do
     {_, host} = List.keyfind(conn.headers, "host", 0)
     {http_handler, _} = Reactor.get_host_handler(:http, host, conn.path, config.hosts)
-    {code, response_headers, body, config} = apply(http_handler, :http, [conn, config])
+    {code, response_headers, body, config} = http_handler.http(conn, config)
     {connection_state, connection_header} = connection_header(conn.headers, http_version)
 
     response_io_list =
